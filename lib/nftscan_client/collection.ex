3cea4207aa9adad91fca1a4d7f228e47ba86f872
Christoph Klocker <christoph@vedanova.com>
2025-05-19 11:12:16 +0200

    removed example

--- lib/nftscan_client/collection.ex
+++ lib/nftscan_client/collection.ex
@@ -3,48 +3,6 @@ defmodule NFTScanClient.Collection do
   Represents a collection from the NFTScan API.
   """
 
-  # Example response:
-
-  # Example response from NFTScan API:
-  # {
-  #   "code": 200,
-  #   "msg": null,
-  #   "data": {
-  #     "contract_address": "0xbadbea1ef8e8ff955069fa5ef8603dbba9a3ce55",
-  #     "name": "Outlaw Dogs",
-  #     "symbol": "OGD",
-  #     "description": "The DODGE CITY OUTLAW DOGS Are A Play-To-Earn NFT Outlaw Gang. No Limit Poker, Shootouts, Bounty Hunting & Treasure Hunts. Welcome To The Wild West!",
-  #     "website": "https://www.outlawdogs.io/",
-  #     "email": null,
-  #     "twitter": "DCOutlawdogs",
-  #     "discord": "https://discord.gg/pPMaCtb9",
-  #     "telegram": null,
-  #     "github": null,
-  #     "instagram": null,
-  #     "medium": null,
-  #     "logo_url": "https://i.seadn.io/gae/KwYqH7tRxMQUJ6Cl-AQVPx21DJJyO0kLzmHXGfMBbDl0L827VGnfT5Fd4dnpsApRwzrNUtsCBeN9_Mzr9LjpO2ozd7V02O5W3L1ypA?w=500&auto=format",
-  #     "banner_url": "https://i.seadn.io/gae/kCYJaOcyIZIWRUFjCcR8qLqz64szN6DJqkADPqrCQ30gIZ5Aqeodhnt2HzpBo9ERXdG_rNGROq4rF61nhtB3PM2WgHWbwM9Lml-Lp2U?w=500&auto=format",
-  #     "featured_url": "https://image.nftscan.com/eth/banner/0xbadbea1ef8e8ff955069fa5ef8603dbba9a3ce55.png",
-  #     "large_image_url": "https://i.seadn.io/gae/5YzcP69gE2dtNOSjEAYhqotcKN8pLQrAbKBQM13QTpqYYilFypQarWtVYwdhI4WN_jkYvqsN9JVYuVOQ5ZWAqp-tSGp4LTeI1ARDNw?w=500&auto=format",
-  #     "attributes": [],
-  #     "erc_type": "erc721",
-  #     "deploy_block_number": 13529442,
-  #     "owner": "0x195edb48580fca079ade888213aea1359889fc4b",
-  #     "verified": false,
-  #     "opensea_verified": false,
-  #     "is_spam": false,
-  #     "royalty": 500,
-  #     "items_total": 866,
-  #     "amounts_total": 866,
-  #     "owners_total": 213,
-  #     "opensea_floor_price": 0.06,
-  #     "opensea_slug": "outlaw-dogs",
-  #     "floor_price": 0.06,
-  #     "collections_with_same_name": ["0xbadbea1ef8e8ff955069fa5ef8603dbba9a3ce55"],
-  #     "price_symbol": "ETH"
-  #   }
-  # }
-
   @type t :: %__MODULE__{
     contract_address: String.t(),
     name: String.t(),
